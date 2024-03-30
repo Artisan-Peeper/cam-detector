70daca417e27aa5c0e9ab9265b7c675ca25b6ab2
Ram <140935795+tcker@users.noreply.github.com>
2024-03-30 23:30:06 +0800

    Update script.js

--- tensorflow/script.js
+++ tensorflow/script.js
@@ -9,16 +9,13 @@ function getUserMediaSupported() {
       navigator.mediaDevices.getUserMedia);
   }
   
-  // If webcam supported, add event listener to button for when user
-  // wants to activate it to call enableCam function which we will 
-  // define in the next step.
   if (getUserMediaSupported()) {
     enableWebcamButton.addEventListener('click', enableCam);
   } else {
     console.warn('getUserMedia() is not supported by your browser');
   }
   
-  // Placeholder function for next step. Paste over this in the next step.
+  // Placeholder function for next step.
   function enableCam(event) {
   }
 
@@ -71,7 +68,6 @@ var children = [];
 function predictWebcam() {
   // Now let's start classifying a frame in the stream.
   model.detect(video).then(function (predictions) {
-    // Remove any highlighting we did previous frame.
     for (let i = 0; i < children.length; i++) {
       liveView.removeChild(children[i]);
     }
